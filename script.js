ed083564358b08481d68ce13582ee33b37e30ea9
Dylan Frank <drfrank22@gmail.com>
2020-09-22 01:06:16 -0400

    created 9am-5pm time blocks using for loop

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-// Today's Date Display
-var todaysDate = $("#currentDay")
+// DISPLAY CURRENT DATE
+var todaysDate = $("#currentDay");
 todaysDate.text(moment().format("MMMM Do YYYY"));
+
+// CREATE TIME CLOCKS FOR ALL TIMES 9AM-5PM
+var hourArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+var timeBlockContainer = $(".container");
+
+for (i = 1; i < hourArray.length; i++) {
+    var timeBlockClones = $("#form").clone();
+    timeBlockClones.attr("id", hourArray[i]); 
+    timeBlockClones.children(".row").attr("style", "white-space: pre-Wrap"); 
+    timeBlockClones.children(".row").children(".hour").text(hourArray[i]); 
+    timeBlockClones.appendTo(timeBlockContainer);
+}
